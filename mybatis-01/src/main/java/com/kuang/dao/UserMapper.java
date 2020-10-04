876f9b8ab840cc676e93d5508ac3e1b3c4568601
zero <1580359334@qq.com>
2020-10-05 03:00:04 +0800

    提交更新

--- mybatis-01/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/kuang/dao/UserMapper.java
@@ -3,16 +3,23 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
+
+    List<User> getUserLike(String s);
+
     //    查询全部用户
     List<User> getUserList();
 
     //    根据ID查询用户
     User getUserById(int id);
 
-    //    insert一个用户
+    //    添加用户
     int addUser(User user);
+
+    //    万能的Map增加用户
+    int addUser2(Map<String, Object> map);
 
     //    修改用户
     int updateUser(User user);
--- mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -5,22 +5,34 @@ import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
 
+
+//    模糊查询
+
+
+
+
+
+
+
+    //查询全部数据
     @Test
     public void test() {
 
-//        第一步获取sqlSession对象
+        //第一步获取sqlSession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         try {
 
-//       方式一：getMapper
-//        UserDao userDao = sqlSession.getMapper(UserDao.class);
-//        List<User> userList = userDao.getUserList();
-//       方式二：
+            //方式一：getMapper
+            //UserDao userDao = sqlSession.getMapper(UserDao.class);
+            //List<User> userList = userDao.getUserList();
+            //方式二：
             List<User> userList = sqlSession.selectList("com.kuang.dao.UserDao.getUserList");
 
             for (User user : userList) {
@@ -38,6 +50,7 @@ public class UserDaoTest {
 
     }
 
+    //    精确查找
     @Test
     public void getUserById() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -51,14 +64,15 @@ public class UserDaoTest {
         sqlSession.close();
     }
 
+    //插入数据
     @Test
     public void addUser() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 
-//        mapper.addUser(new User(10,"haha","zxz"));
-//        抛出异常
+        //mapper.addUser(new User(10,"haha","zxz"));
+        //抛出异常
         try {
             int res = mapper.addUser(new User(1, "小明", "中国"));
             if (res > 0) {
@@ -66,7 +80,7 @@ public class UserDaoTest {
             } else {
                 System.out.println("插入失败");
             }
-//        提交事务
+            //提交事务
             sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -77,6 +91,38 @@ public class UserDaoTest {
 
     }
 
+    //Map插入数据
+    @Test
+    public void addUser2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        map.put("userid", 7);
+        map.put("username", "老张");
+        map.put("userpwd", "中国");
+
+
+        int res = mapper.addUser2(map);
+        if (res > 0) {
+            System.out.println(map);
+            System.out.println("插入成功！");
+        } else {
+            System.out.println(map);
+            System.out.println("插入失败");
+        }
+
+        //提交事务
+        sqlSession.commit();
+        //关闭SqlSession
+        sqlSession.close();
+
+    }
+
+
+    //修改数据
     @Test
     public void updateUser() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -92,12 +138,13 @@ public class UserDaoTest {
         } else {
             System.out.println("修改失败");
         }
-//        提交事务
+        //提交事务
         sqlSession.commit();
         //关闭SqlSession
         sqlSession.close();
     }
 
+    //删除数据
     @Test
     public void deletUserById() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
